266b11f2c246acfe24911339a67c0df7903e0b27
Joaquin-Sanchez-Tonani <joaquinsancheztonani15@gmail.com>
2023-07-12 18:15:10 -0300

    big design changes

--- src/main/header/body/aboutMe/aboutMe.jsx
+++ src/main/header/body/aboutMe/aboutMe.jsx
@@ -8,36 +8,28 @@ function AboutMe(){
                 <h3 className="tittle-about-me">
                     About Me
                 </h3>
-                <div className="contenedor-1">
-                    <p className="text-about-me"> 
-                        My name is <strong className="strong">Joaquin</strong>,
-                        I'm 20 years old and I was born in <strong className="strong">Rosario</strong>,
-                        city of <strong className="strong">Argentina</strong>. 
-                    </p>
-                </div>
-                <div className="contenedor-1">
-                    <p className="text-about-me"> 
-                        My <strong className="strong">objective</strong> is being able to work and keep learning. 
-                        Buff <strong className="strong">my current knowledge</strong>,
-                         improve my <strong className="strong">english</strong> and learn other <strong className="strong">new technologies</strong>.
-                    </p>
-                </div>
-                <div className="contenedor-1">
-                    <p className="text-about-me"> 
-                        I have a <strong className="strong">lot of experience</strong> but I did not have the <strong className="strong">opportunity to work.</strong>
-                    </p>
-                </div>
-                <div className="contenedor-1">
-                    <p className="text-about-me"> 
-                        My hobbies are <strong className="strong">reading manga</strong>,
-                         listening to <strong className="strong">music</strong>,
-                         making <strong className="strong">physical activity</strong> and of course <strong className="strong">drinking coffee.</strong>
-                    </p>
-                </div>
-                <div className="contenedor-1">
-                    <p className="text-about-me"> 
-                    <strong className="strong">Web developer</strong> since 2022, knowledge about <strong className="strong"> JavaScript, HTML, CSS and React framework. </strong>
-                    </p>
+                <div className="cont-about">
+                    <div className="contenedor-1">
+                        <p className="text-about-me"> 
+                                                    Aquí tienes el texto en inglés con las etiquetas <strong className="strong"></strong> agregadas a las palabras o partes importantes sin repetir las etiquetas en el mismo texto:
+
+                            My name is <strong className="strong">Joaquin</strong>, and I was born in <strong className="strong">Rosario, Argentina</strong>. I'm <strong className="strong">20 years old</strong> and have been a <strong className="strong">programmer</strong> for the past 2 years. Programming has been my passion, and I enjoy the creative process of solving problems and building innovative solutions.
+
+                            Apart from programming, I have a wide range of hobbies and interests. I love <strong className="strong">reading mangas</strong>, watching <strong className="strong">anime</strong> or <strong className="strong">series</strong>.
+
+                            <strong className="strong"> Music</strong> is also a big part of my life. I find solace and inspiration in various genres, and it often fuels my creativity while programming. Whether it's listening to my favorite tunes or discovering new artists, music has a way of connecting with me on a deeper level.
+
+                            To maintain a healthy balance in my life, I engage in physical activities regularly. Whether it's going for a run, or hitting the <strong className="strong">gym</strong>, I find that exercise not only keeps me physically fit but also helps me clear my mind and stay focused.
+
+                            And of course, I am an avid <strong className="strong">coffee lover</strong>. There's something comforting and invigorating about sipping a cup of coffee while working on a challenging coding project or simply enjoying a moment of relaxation.
+
+                            Although I have gained a lot of experience through personal projects and self-learning, I haven't had the opportunity to work professionally yet. However, I am eager to apply my skills and knowledge in a professional setting, collaborate with a team, and contribute to meaningful projects.
+
+                            I am <strong className="strong">driven</strong>, curious, and always seeking to expand my knowledge in the world of programming. I am excited about the possibilities that lie ahead and the prospect of making a significant impact through my work.
+
+                            That's a little glimpse into who I am, Joaquin, the passionate programmer with diverse interests and a hunger for new experiences.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/main/header/body/education/education.jsx
+++ src/main/header/body/education/education.jsx
@@ -8,24 +8,38 @@ function Education(){
                 <div  className="contendor-edu">
                     <h3 className="tittle-education">Education</h3>
                     <div className="education-contenedor">
+                        
                         <div className="education-apartado">
+                            <div class="esquina-izquierda"></div>
+                            <div class="esquina-derecha"></div>
                             <h3 className="texto-edu">UNIVERSITY CERTIFICATE IN PROGRAMMING</h3>
-                            <p>+ FIRST YEAR | Current</p></div>
+                            <p><i class="fi fi-rr-angle-small-right">FIRST YEAR | Current</i></p></div>
                         <div className="education-apartado">
+                            <div class="esquina-izquierda"></div>
+                            <div class="esquina-derecha"></div>
                             <h3 className="texto-edu">F.A.S.T.A. N°1158 <h3 className="texto-edu2">(HIGH SCHOOL)</h3></h3>
-                            <p>+ Bachelor's degree in economics and administration | 2016 - 2021</p></div>
+                            <p><i class="fi fi-rr-angle-small-right">Bachelor's degree in economics and administration | 2016 - 2021</i></p></div>
                         <div className="education-apartado">
+                            <div class="esquina-izquierda"></div>
+                            <div class="esquina-derecha"></div>
                             <h3 className="texto-edu">PROGRAMMING COURSE <h3 className="texto-edu2">#Yo Programo</h3> </h3>
-                            <p>+ Pyton<br></br>+ Objects</p></div>
+                            <p><i class="fi fi-rr-angle-small-right">Python</i><br></br><i class="fi fi-rr-angle-small-right">Objects</i></p></div>
                         <div className="education-apartado">
-                            <h3 className="texto-edu">ENGLISH COURSE <br></br><h3 className="texto-edu2">#Basic program of english</h3> </h3>
-                            <p>+ Level A2</p></div>
+                            <div class="esquina-izquierda"></div>
+                            <div class="esquina-derecha"></div>
+                            <h3 className="texto-edu">ENGLISH COURSE <br></br><h3 className="texto-edu2">#Basic program of english</h3></h3>
+                            <p><i class="fi fi-rr-angle-small-right">Level A2</i></p>
+                            </div>
                         <div className="education-apartado">
+                            <div class="esquina-izquierda"></div>
+                            <div class="esquina-derecha"></div>
                             <h3 className="texto-edu">REACT COURSE <h3 className="texto-edu2">freeCodeCamp</h3></h3>
-                            <p>+ Basic program of React. <br></br>+ UseState<br></br>+ Components</p></div>
+                            <p><i class="fi fi-rr-angle-small-right">Basic program of React.</i> <br></br><i class="fi fi-rr-angle-small-right">UseState</i><br></br><i class="fi fi-rr-angle-small-right">Components</i></p></div>
                         <div className="education-apartado">
+                            <div class="esquina-izquierda"></div>
+                            <div class="esquina-derecha"></div>
                             <h3 className="texto-edu">JAVASCRIPT COURSE <h3 className="texto-edu2">Lucas Dalto</h3></h3>
-                            <p>+ Mid level<br></br>+ Fetch<br></br>+ Document Object Model</p></div>
+                            <p><i class="fi fi-rr-angle-small-right">Mid level</i> <br></br><i class="fi fi-rr-angle-small-right">Fetch</i> <br></br><i class="fi fi-rr-angle-small-right">Document Object Model</i></p></div>
                     </div>
                 </div>
             </div>
