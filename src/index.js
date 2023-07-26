c74e8423d387e76911ebbb122c6c73c1380e887a
Joaquin-Sanchez-Tonani <joaquinsancheztonani15@gmail.com>
2023-07-26 05:08:54 -0300

    aaa

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculadora/calculator';
 import Header from './header-proyects/header';
 
-const rutas = (
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route
-      path="/project"
-      element={
+const Root = () => (
+  <React.StrictMode>
+    <Router>
+      <Route exact path="/" component={App} />
+      <Route path="/project" component={() => (
         <div>
-          <Header />
+          <Header /> 
           <Calculator />
         </div>
-      }
-    />
-  </Routes>
-);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router>{rutas}</Router>
+      )} />
+    </Router>
   </React.StrictMode>
 );
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
