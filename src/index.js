f8ebb188359d2e36f399c9ba51c5c79adcb93381
Joaquin-Sanchez-Tonani <joaquinsancheztonani15@gmail.com>
2023-07-26 05:36:19 -0300

    prueba

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Importar desde react-dom/client
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import { createBrowserRouter , RouterProvider } from 'react-router-dom';
 import Calculator from './components/calculadora/calculator';
 import Header from './header-proyects/header';
 
-const rutas = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/project',
-    element:  (
-    <div>
-      <Header /> 
-      <Calculator />
-    </div>)
-  }
-])
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={rutas}></RouterProvider>
-  </React.StrictMode>
+const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/project" element={(
+        <div>
+          <Header /> 
+          <Calculator />
+        </div>
+      )} />
+    </Routes>
+  </Router>
 );
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<Root />);
