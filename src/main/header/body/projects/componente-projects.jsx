23aba2ce1386b960520598857e968239cd43ed79
Joaqui Dario Sanchez Tonani <joaquinsancheztonani15@gmail.com>
2023-10-06 22:38:31 -0300

    Projects modification

--- src/main/header/body/projects/componente-projects.jsx
+++ src/main/header/body/projects/componente-projects.jsx
@@ -2,19 +2,12 @@ import React from "react";
 import './componente-projects.css';
 
 function ComponenteProjects(props){
-    const lines = props.text.split('/n')
 
     return(
-        <a className="project-router" href={`${props.ruta}`}>
-        <div className="marco1">
-            <h3 className="h3-absolute">{props.name}</h3>
-            <div className="marco-imagen">
-                <img className="project-img" src={require(`./${props.img}.png`)} alt={props.name} />
+        <a className="proyecto-div" target="_black" href={`${props.ruta}`}>
+            <div>
+                <h1 className="project-titulo">{props.name}</h1>
             </div>
-            <div className="marco-p">   
-                <p><strong className="h5-title"></strong><br/> </p>{ lines.map((line) => (<p key={line} className="lineas-projects">+ {line}</p>)) }
-            </div>
-        </div>
         </a> 
     )
 }
--- src/main/header/body/projects/projects.jsx
+++ src/main/header/body/projects/projects.jsx
@@ -4,11 +4,22 @@ import ComponenteProjects from "./componente-projects";
 
 function Projects(){
     return(
-    <div className="espaciado">
-        <div id="projects" className="projects-contenedor">
-            <h3  className="tittle-education">Projects</h3>
-            <div className="column-project">
-                <ComponenteProjects ruta='https://joaquin-sanchez-tonani-ecommerce.netlify.app/' img='ecommerce' name='E-commerce' text= 'E-commerce site'   />
+    // <div className="espaciado">
+    //     <div id="projects" className="projects-contenedor">
+    //         <h3  className="tittle-education">Projects</h3>
+    //         <div className="column-project">
+    //             <ComponenteProjects ruta='https://joaquin-sanchez-tonani-ecommerce.netlify.app/' img='ecommerce' name='E-commerce' text= 'E-commerce site'   />
+    //             <ComponenteProjects ruta='project' img='calculadora' name='Calculator' text= 'Add /n Subtract /n Divide /n Multiply'   />
+    //             <ComponenteProjects ruta='converter' img='converter' name='Converter' text='Converter currency to currency./n This works by API key'/>
+    //         </div>
+    //     </div>
+    // </div>
+    <div id="projects" className="apartado-project">
+                    <h3 className="titulo-projects-h3">Projects</h3>
+
+        <div  className="grid-projects">
+            <div className="componentes">
+                <ComponenteProjects ruta='https://joaquin-sanchez-tonani-ecommerce.netlify.app/' img='ecommerce' name='E-commerce site' text= 'E-commerce site'   />
                 <ComponenteProjects ruta='project' img='calculadora' name='Calculator' text= 'Add /n Subtract /n Divide /n Multiply'   />
                 <ComponenteProjects ruta='converter' img='converter' name='Converter' text='Converter currency to currency./n This works by API key'/>
             </div>
